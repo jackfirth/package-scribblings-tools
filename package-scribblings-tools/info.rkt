2ce494d6e12d218205b7010ad7b315f694aea4a1
Jack Firth <jackhfirth@gmail.com>
2015-03-31 15:54:09 -0700

    Define exports

--- package-scribblings-tools/info.rkt
+++ package-scribblings-tools/info.rkt
@@ -1,1 +1,8 @@
 #lang info
+
+
+(define name "package-scribblings-tools")
+
+
+(define scribblings
+  '(("scribblings/main.scrbl" () (library) "package-scribblings-tools")))
--- package-scribblings-tools/main.rkt
+++ package-scribblings-tools/main.rkt
@@ -1,3 +1,8 @@
 #lang racket
 
 (require mischief)
+
+(require/provide "defpredicate.rkt"
+                 "example-evaluator.rkt"
+                 "module-title.rkt"
+                 "source-code.rkt")
