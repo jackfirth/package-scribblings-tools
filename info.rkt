9b0c2e2185077fccc5e1f78f8d205fc1a7c15170
Spencer Florence <florence@ccs.neu.edu>
2015-10-03 10:37:59 -0500

    removed mischief

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base" "rackunit-lib" "mischief" "scribble-lib" "scribble-text-lib"))
+(define deps '("base" "rackunit-lib" "scribble-lib" "scribble-text-lib"))
 (define build-deps '("cover"
                      "rackunit-lib"
                      "racket-doc"))
