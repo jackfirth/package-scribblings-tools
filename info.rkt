50b26fb4daae990579eb8e0d96ffb1cdd6de786c
Jack Firth <jackhfirth@gmail.com>
2015-10-03 09:38:16 -0700

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -3,5 +3,6 @@
 (define collection 'multi)
 (define deps '("base" "rackunit-lib" "scribble-lib" "scribble-text-lib"))
 (define build-deps '("cover"
+                     "cover-coveralls"
                      "rackunit-lib"
                      "racket-doc"))
